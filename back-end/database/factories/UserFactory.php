62615ed81393496f4c4e7b55d82ee97459724efa
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-16 12:30:04 +0100

    [Laravel]add sanctum and basic auth

--- back-end/database/factories/UserFactory.php
+++ back-end/database/factories/UserFactory.php
@@ -18,7 +18,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->name(),
+            'last_name' => $this->faker->name(),
+            'cin' => $this->faker->numberBetween(100000000, 999999999),
+            'birth_date' => $this->faker->date(),
+            'phone_number' => $this->faker->numberBetween(100000000, 999999999),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->integer('cin');
+            $table->date('birth_date');
+            $table->integer('phone_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
