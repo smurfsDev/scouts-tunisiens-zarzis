9ee1a67504321a59ac0ae11343acdf1bf5ba3780
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-16 21:18:07 +0100

    add icons and use interceptor

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -5,8 +5,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import interceptorsSetup from './helpers/interceptors';
 Vue.config.productionTip = false;
+interceptorsSetup()
 
 new Vue({
   router,
