fdf0b34f40d2975ccc2d61c9caf3796d424e1bd4
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-18 01:54:05 +0100

    [Vue]create and use vuelidate

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -5,12 +5,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import Vuelidate from './plugins/vuelidate';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import interceptorsSetup from './helpers/interceptors';
 Vue.config.productionTip = false;
 interceptorsSetup()
 
 new Vue({
+  Vuelidate,
   router,
   store,
   vuetify,
