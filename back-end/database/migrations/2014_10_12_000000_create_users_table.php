3a9cf9b31db04b8a861b4a288eb001065561db1a
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-18 16:36:27 +0100

    [Vue]set cin birthdate phone number to nullable

--- back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,9 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->integer('cin');
-            $table->date('birth_date');
-            $table->integer('phone_number');
+            $table->integer('cin')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->integer('phone_number')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
