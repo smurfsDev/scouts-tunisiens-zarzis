adcfa408f40e6291ff6a51c0d7642968970ecf70
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-19 01:52:30 +0100

    get all roles

--- back-end/app/Http/Controllers/RoleController.php
+++ back-end/app/Http/Controllers/RoleController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    //
+    public function index(){
+        $roles = Role::where("name","<>","superAdmin")->get();
+        return response()->json($roles);
+    }
 }
