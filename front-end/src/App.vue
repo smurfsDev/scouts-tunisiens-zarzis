6c35b22732f1a5b452967cd87b2214af56fb266d
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-19 01:54:13 +0100

    divide app.vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,71 +1,20 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-      <div class="text-center">
-        <v-menu open-on-hover top offset-y offset-x offset-overflow>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="primary" dark v-bind="attrs" v-on="on">
-              Compte <v-icon>mdi-account</v-icon>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item v-if="this.$store.getters.isLoggedIn">
-              <h6
-                ><v-icon>mdi-account</v-icon>{{ $store.getters.authUser.first_name+" "+$store.getters.authUser.last_name }}</h6
-              >
-            </v-list-item>
-            <v-list-item v-if="!this.$store.getters.isLoggedIn">
-              <router-link class="button" to="/login"
-                ><v-icon>mdi-account-arrow-right</v-icon>Connecter</router-link
-              >
-            </v-list-item>
-            <v-list-item v-if="this.$store.getters.isLoggedIn">
-              <router-link class="button" to="/logout"
-                ><v-icon>mdi-account-arrow-left</v-icon>Deconnecter</router-link
-              >
-            </v-list-item>
-            <v-list-item v-if="!this.$store.getters.isLoggedIn">
-              <router-link class="button" to="/register"
-                ><v-icon>mdi-account-plus</v-icon>S'inscrire</router-link
-              >
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </div>
-    </v-app-bar>
-
+  <v-app >
+    
+    <navbar/>
     <v-main>
-      <router-view />
+      <router-view class="text-right" />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import navbar from './components/NavBar.vue'
 export default {
   name: "App",
-
+  components: {
+    navbar
+  },
   data: () => ({
     //
   }),
