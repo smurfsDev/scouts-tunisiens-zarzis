6f0d729ee518af30b18a84ef3634832d439a6e2f
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-19 01:54:13 +0100

    [Vue]divide app.vue

---
+++ front-end/src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+      <div class="text-center">
+        <v-menu open-on-hover top offset-y offset-x offset-overflow>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on">
+              الحساب <v-icon>mdi-account</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item v-if="this.$store.getters.isLoggedIn">
+              <h6
+                ><v-icon>mdi-account</v-icon>{{ $store.getters.authUser.first_name+" "+$store.getters.authUser.last_name }}</h6
+              >
+            </v-list-item>
+            <v-list-item v-if="!this.$store.getters.isLoggedIn">
+              <router-link class="button" to="/login"
+                ><v-icon>mdi-account-arrow-right</v-icon>تسجيل الدخول</router-link
+              >
+            </v-list-item>
+            <v-list-item v-if="this.$store.getters.isLoggedIn">
+              <router-link class="button" to="/logout"
+                ><v-icon>mdi-account-arrow-left</v-icon>الخروج</router-link
+              >
+            </v-list-item>
+            <v-list-item v-if="!this.$store.getters.isLoggedIn">
+              <router-link class="button" to="/register"
+                ><v-icon>mdi-account-plus</v-icon>انشاء حساب</router-link
+              >
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
