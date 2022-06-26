05c9747f1c7a3b9c6b21022131138f561d58f188
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-26 16:04:37 +0100

    [Laravel] add troupe role user relations to roleuser

--- back-end/app/Models/RoleUser.php
+++ back-end/app/Models/RoleUser.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class RoleUser extends Pivot
 {
     use HasFactory;
+    public $incrementing = true;
+
+    public function troupe()
+    {
+        return $this->belongsTo(Troupe::class,'troupe_id');
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
