81f83eb6485e209d27cd563073d43295ed077bfd
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-26 16:05:24 +0100

    [Laravel] add ename 'english name' to role

--- back-end/database/seeders/RoleSeeder.php
+++ back-end/database/seeders/RoleSeeder.php
@@ -17,26 +17,33 @@ class RoleSeeder extends Seeder
     {
         Role::create([
             'name' => 'superAdmin',
+            'ename' => 'Super Admin',
         ]);
         Role::create([
             'name' => 'قيادة الفوج',
+            'ename' => 'Leadership',
         ]);
         Role::create([
             'name' => 'قائد وحدة',
+            'ename' => 'Unit Leader',
         ]);
         Role::create([
             'name' => 'ملازم قائد وحدة',
+            'ename' => 'Unit Sub Leader',
         ]);
         Role::create([
             'name' => 'معين قائد وحدة',
+            'ename' => 'Unit Assigned Leader',
         ]);
 
         Role::create([
             'name' => 'فرد',
+            'ename' => 'Member',
         ]);
 
         Role::create([
             'name' => 'ولي',
+            'ename' => 'Parent',
         ]);
 
         // Role::factory(4)->create();
