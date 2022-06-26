e6cc0a4b32967d87640f3791cdf187f2b4b0cece
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-26 17:17:26 +0100

    [Vue] fix layout

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <v-app >
-    
-    <navbar/>
-    <v-main>
-      <router-view class="text-right" />
+  <v-app>
+    <navbar />
+    <v-main class="grey lighten-3">
+      <v-container fluid class="my-4">
+          <router-view class="text-right" />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import navbar from './components/NavBar.vue'
+import navbar from "./components/NavBar.vue";
 export default {
   name: "App",
   components: {
-    navbar
+    navbar,
   },
   data: () => ({
     //
