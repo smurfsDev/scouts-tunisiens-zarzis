9467263e229d4d2ee3cff0c535d957a0714d2de4
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-28 00:40:07 +0100

    [Vue]create and user manage members route

---
+++ front-end/src/router/routes/UnitLeader/manageMembers.js
@@ -0,0 +1,10 @@
+const manageMembers = () => import("@/views/UnitLeader/manageMembers.vue");
+
+export default [
+  {
+    path: "/manageMembers",
+    component: manageMembers,
+    name: "manageMembers",
+    meta: {requiresUnitLeader: true},
+  },
+];
