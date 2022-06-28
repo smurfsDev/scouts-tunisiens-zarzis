d903de3466a9552653d0b649264e25c6ecc11ce6
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-28 05:39:51 +0100

    [Vue]create requestVerifyemail main component and routes

---
+++ front-end/src/router/routes/auth/requestVerifyEmail.js
@@ -0,0 +1,9 @@
+const requestVerifyEmail = () => import("../../../views/Auth/requestVerifyEmail.vue");
+
+export default [
+  {
+    path: "/request-verify-email",
+    component: requestVerifyEmail,
+    name: "requestVerifyEmail",
+  },
+];
