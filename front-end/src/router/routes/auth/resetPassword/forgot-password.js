f263a447b253c97d3e4b7ac1b92bfbf5e800123b
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-29 01:42:28 +0100

    [Vue] create forgot-password route and mainComponent

---
+++ front-end/src/router/routes/auth/resetPassword/forgot-password.js
@@ -0,0 +1,8 @@
+const forgotPassword = () => import("@/views/Auth/resetPassword/forgotPassword.vue");
+export default [
+  {
+    path: "/forgot-password",
+    component: forgotPassword,
+    name: "forgotPassword",
+  },
+];
