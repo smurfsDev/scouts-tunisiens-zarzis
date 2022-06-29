3292516b0565990236b313b04f67d0ec79ac8a37
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-29 01:35:16 +0100

    [Laravel] create resetCodePassword model and table

---
+++ back-end/app/Models/ResetCodePassword.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ResetCodePassword extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'email';
+    protected $fillable = [
+        'email',
+        'code',
+        'created_at',
+    ];
+    protected $dates = [
+        'created_at',
+    ];
+}
