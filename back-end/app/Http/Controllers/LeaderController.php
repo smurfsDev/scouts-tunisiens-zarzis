ba9677b0efb8a33a022e8b95e5613126347aea5f
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-03 20:52:53 +0100

    [Laravel] order leaders by status, add unit assigned leader verification on accept

--- back-end/app/Http/Controllers/LeaderController.php
+++ back-end/app/Http/Controllers/LeaderController.php
@@ -15,9 +15,13 @@ class LeaderController extends Controller
      */
     public function index()
     {
-        $leaders = RoleUser::with('troupe','role','user')->whereHas('role', function ($query) {
-            $query->where('ename', 'like', '%Leader%');
-        })->get();
+        $leaders = RoleUser::with('troupe', 'role', 'user', 'responsability')
+            ->whereHas('role', function ($query) {
+                $query->where('ename', 'like', '%Leader%');
+            })
+            ->orderBy('status', 'ASC')
+            ->orderBy('created_at', 'DESC')
+            ->get();
         return response()->json($leaders, 200);
     }
 
@@ -68,20 +72,38 @@ class LeaderController extends Controller
 
     public function accept($id)
     {
-        $leader = RoleUser::with('troupe')
+        $leader = RoleUser::with('troupe', 'role')
             ->with('role')->find($id);
-        $otherLeaders = RoleUser
-            ::where('troupe_id', $leader->troupe_id)
-            ->where('role_id', $leader->role_id)
-            ->where('user_id', '!=', $leader->user_id)
-            ->where('status', 1)
+        if ($leader->role->ename == 'Unit Assigned Leader') {
+            $otherLeaders = RoleUser
+                ::where('troupe_id', $leader->troupe_id)
+                ->where('role_id', $leader->role_id)
+                ->where('responsability_id', $leader->responsability_id)
+                ->where('user_id', '!=', $leader->user_id)
+                ->where('status', 1)
 
-            ->get();
-        if (count($otherLeaders) > 0) {
-            return response()->json(['error' => 'يوجد '.$leader->role->name.' معين ل'.$leader->troupe->name.'.'], 400);
+                ->get();
+            if (count($otherLeaders) > 0) {
+                return response()->json(['error' => 'يوجد ' . $leader->role->name . ' معين ل' . $leader->troupe->name . ' '.$leader->responsability[0]->name.'.'], 400);
+            } else {
+                $leader->status = 1;
+            }
+            $leader->save();
+        } else {
+            $otherLeaders = RoleUser
+                ::where('troupe_id', $leader->troupe_id)
+                ->where('role_id', $leader->role_id)
+                ->where('user_id', '!=', $leader->user_id)
+                ->where('status', 1)
+
+                ->get();
+            if (count($otherLeaders) > 0) {
+                return response()->json(['error' => 'يوجد ' . $leader->role->name . ' معين ل' . $leader->troupe->name . '.'], 400);
+            } else {
+                $leader->status = 1;
+            }
+            $leader->save();
         }
-        $leader->status = 1;
-        $leader->save();
         return response()->json($leader, 200);
     }
 
