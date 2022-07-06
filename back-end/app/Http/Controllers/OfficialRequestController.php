ef89420523532452bd3058901996be958bd444cc
raed charrad <raed.charrad91@gmail.com>
2022-07-06 17:02:54 +0100

    [Laravel] add create function to officialRequestController

--- back-end/app/Http/Controllers/OfficialRequestController.php
+++ back-end/app/Http/Controllers/OfficialRequestController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OfficialRequest;
 use Illuminate\Http\Request;
 
 class OfficialRequestController extends Controller
 {
-    //
+    public function create(Request $request){
+        $userId = $request->user()->id;
+        $object = $request->input('object');
+        $receiver = $request->input('receiver');
+        $content = $request->input('content');
+        $data = array(
+            "object"=> $object,
+            "userId"=> $userId,
+            "idDestinataire"=> $userId,
+            "receiver"=> $receiver,
+            "content"=> $content,
+        );
+        $OfficialRequest = OfficialRequest::create($data);
+        if ($OfficialRequest) {
+            return response()->json([
+                'data' => [
+                    'message' => 'Success',
+                    'id' => $OfficialRequest->id,
+                    'attributes' => $OfficialRequest
+                ]
+            ], 201);
+        }
+        return "Success";
+    }
 }
