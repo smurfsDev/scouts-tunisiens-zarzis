3d7ef0d0990ef81025d97e124d81e98774823c0f
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-08 14:29:09 +0100

    [Laravel] materiel factory/seeder/model

--- back-end/database/seeders/DatabaseSeeder.php
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(TroupeSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(MaterielSeeder::class);
     }
 }
