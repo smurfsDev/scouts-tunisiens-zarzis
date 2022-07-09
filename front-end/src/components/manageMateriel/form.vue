31725d521bdfce1a6bf2da1cdbde4a17ebdb7660
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-09 03:51:04 +0100

    [Vue] add categories on create material

--- front-end/src/components/manageMateriel/form.vue
+++ front-end/src/components/manageMateriel/form.vue
@@ -45,6 +45,84 @@
                   class="mt-3"
                 ></v-text-field>
               </v-col>
+              <v-col>
+                <v-combobox
+                  v-model="form.tags"
+                  :filter="filter"
+                  :hide-no-data="!search"
+                  :items="items"
+                  :search-input.sync="search"
+                  hide-selected
+                  label="Search for an option"
+                  multiple
+                  small-chips
+                  solo
+                >
+                  <template v-slot:no-data>
+                    <v-list-item>
+                      <span class="subheading">Create</span>
+                      <v-chip
+                        :color="`${colors[nonce - 1]} lighten-3`"
+                        label
+                        small
+                      >
+                        {{ search }}
+                      </v-chip>
+                    </v-list-item>
+                  </template>
+                  <template
+                    v-slot:selection="{ attrs, item, parent, selected }"
+                  >
+                    <v-chip
+                      v-if="item === Object(item)"
+                      v-bind="attrs"
+                      :color="`${item.color} lighten-3`"
+                      :input-value="selected"
+                      label
+                      small
+                    >
+                      <span class="pr-2">
+                        {{ item.text }}
+                      </span>
+                      <v-icon small @click="parent.selectItem(item)">
+                        $delete
+                      </v-icon>
+                    </v-chip>
+                  </template>
+                  <template v-slot:item="{ index, item }">
+                    <v-text-field
+                      v-if="editing === item"
+                      v-model="editing.text"
+                      autofocus
+                      flat
+                      background-color="transparent"
+                      hide-details
+                      solo
+                      @keyup.enter="edit(index, item)"
+                    ></v-text-field>
+                    <v-chip
+                      v-else
+                      :color="`${item.color} lighten-3`"
+                      dark
+                      label
+                      small
+                    >
+                      {{ item.text }}
+                    </v-chip>
+                    <v-spacer></v-spacer>
+                    <v-list-item-action @click.stop>
+                      <v-btn icon @click.stop.prevent="edit(index, item)">
+                        <v-icon>{{
+                          editing !== item ? "mdi-pencil" : "mdi-check"
+                        }}</v-icon>
+                      </v-btn>
+                      <v-btn icon @click.stop.prevent="delet(item)">
+                        <v-icon>mdi-delete</v-icon>
+                      </v-btn>
+                    </v-list-item-action>
+                  </template>
+                </v-combobox>
+              </v-col>
             </v-row>
           </v-container>
         </v-card-text>
@@ -95,13 +173,38 @@ export default {
       type: Object,
       default: () => ({}),
     },
+    editm: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  created() {
+    this.getCategories();
   },
   data: () => ({
     form: {
       name: "",
       description: "",
       quantity: "",
+      tags: [],
     },
+    activator: null,
+    attach: null,
+    colors: ["green", "purple", "indigo", "cyan", "teal", "orange"],
+    editing: null,
+    editingIndex: -1,
+    items: [],
+    nonce: 1,
+    menu: false,
+    model: [
+      {
+        text: "Foo",
+        color: "blue",
+      },
+    ],
+    x: 0,
+    search: null,
+    y: 0,
   }),
   methods: {
     reset() {
@@ -115,7 +218,7 @@ export default {
       if (this.$v.$invalid) {
         return;
       }
-      if (this.edit == false) {
+      if (this.editm == false) {
         this.$axios
           .post("/materiel/", this.form)
           .then(() => {
@@ -153,6 +256,43 @@ export default {
             });
             this.reset();
           });
+      }
+    },
+    edit(index, item) {
+      if (!this.editing) {
+        this.editing = item;
+        this.editingIndex = index;
+      } else {
+        this.$axios
+          .put("/categorie-materiel/" + this.editing.id, { name: this.editing.text })
+          .then(() => this.getCategories());
+        this.editing = null;
+        this.editingIndex = -1;
+      }
+    },
+    filter(item, queryText, itemText) {
+      if (item.header) return false;
+
+      const hasValue = (val) => (val != null ? val : "");
+
+      const text = hasValue(itemText);
+      const query = hasValue(queryText);
+
+      return (
+        text.toString().toLowerCase().indexOf(query.toString().toLowerCase()) >
+        -1
+      );
+    },
+    getCategories() {
+      this.$axios.get("/categorie-materiel").then((response) => {
+        this.items = response.data;
+      });
+    },
+    delet(item) {
+      if (confirm("هل تريد حذف فئة الاثاث " + item.text + "؟")) {
+        this.axios.delete("/categorie-materiel/" + item.id).then(() => {
+          this.getCategories();
+        });
       }
     },
   },
@@ -198,7 +338,33 @@ export default {
             name: "",
             description: "",
             quantity: "",
+            tags: [],
           };
+    },
+    model(val, prev) {
+      if (val.length === prev.length) return;
+
+      this.model = val.map((v) => {
+        if (typeof v === "string") {
+          v = {
+            text: v,
+            color: this.colors[this.nonce - 1],
+          };
+          this.$axios
+            .post("/categorie-materiel/", { name: v.text })
+            .then(() => {
+              this.getCategories();
+            });
+
+          this.nonce++;
+        }
+        if (this.editing) {
+          this.$axios
+            .put("/categorie-materiel/" + this.editing.id, { name: v.text })
+            .then(() => this.getCategories());
+        }
+        return v;
+      });
     },
   },
 };
