6da0f6c66b49c4b15294193abfd021366e3dcff5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-09 14:43:53 +0100

    [Laravel] add onDelete cascade

--- back-end/database/migrations/2022_07_08_192036_categorie_materiel_materiels.php
+++ back-end/database/migrations/2022_07_08_192036_categorie_materiel_materiels.php
@@ -16,9 +16,11 @@ return new class extends Migration
         Schema::create('categorie_materiel_materiel', function (Blueprint $table) {
             $table->id();
             $table->foreignId('categorie_materiel_id');
-            $table->foreign('categorie_materiel_id')->references('id')->on('categorie_materiels');
+            $table->foreign('categorie_materiel_id')->references('id')->on('categorie_materiels')
+                ->onDelete('cascade');
             $table->foreignId('materiel_id');
-            $table->foreign('materiel_id')->references('id')->on('materiels');
+            $table->foreign('materiel_id')->references('id')->on('materiels')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
