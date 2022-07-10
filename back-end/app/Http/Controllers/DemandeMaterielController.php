d66c6c03ddbbd1b7dff7b8bfc9c60cde65d4a5ff
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-10 14:05:44 +0100

    [Laravel] update sentDemandes/store/destroy/update methods

--- back-end/app/Http/Controllers/DemandeMaterielController.php
+++ back-end/app/Http/Controllers/DemandeMaterielController.php
@@ -17,6 +17,22 @@ class DemandeMaterielController extends Controller
         //
     }
 
+    public function sentDemandes(Request $request)
+    {
+        $demandes = DemandeMateriel::where('user_id', $request->user()->id)->with('responsable')->get();
+        if ($demandes->count() > 0) {
+            return response()->json(
+                $demandes,
+                200
+            );
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Aucune demande envoyée'
+            ], 404);
+        }
+    }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -25,7 +41,23 @@ class DemandeMaterielController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $demande = DemandeMateriel::create([
+            'user_id' => $request->user()->id,
+            'responsable_id' => 6,
+            'date_demande' => $request->date_demande,
+            'message' => $request->message,
+        ]);
+        if ($demande) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Demande envoyée'
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erreur lors de l\'envoi de la demande'
+            ], 500);
+        }
     }
 
     /**
@@ -46,9 +78,24 @@ class DemandeMaterielController extends Controller
      * @param  \App\Models\DemandeMateriel  $demandeMateriel
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, DemandeMateriel $demandeMateriel)
+    public function update(Request $request,$demandeMateriel)
     {
-        //
+        $demande = DemandeMateriel::find($demandeMateriel);
+        if ($demande) {
+            $demande->update([
+                'date_demande' => $request->date_demande,
+                'message' => $request->message,
+            ]);
+            return response()->json([
+                'success' => true,
+                'message' => 'Demande mise à jour'
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erreur lors de la mise à jour de la demande'
+            ], 500);
+        }
     }
 
     /**
@@ -57,8 +104,20 @@ class DemandeMaterielController extends Controller
      * @param  \App\Models\DemandeMateriel  $demandeMateriel
      * @return \Illuminate\Http\Response
      */
-    public function destroy(DemandeMateriel $demandeMateriel)
+    public function destroy($demandeMateriel)
     {
-        //
+        $demande = DemandeMateriel::find($demandeMateriel);
+        if ($demande) {
+            $demande->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'Demande supprimée'
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erreur lors de la suppression de la demande'
+            ], 500);
+        }
     }
 }
