f7d5805bb73503983c261b5c6602666b044f17fc
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-07-13 11:42:47 +0100

    [Vue] show and status managedemandeMateriel

--- front-end/src/views/UnitAssignedLeader/materiel-manager/manageDemandeMateriel.vue
+++ front-end/src/views/UnitAssignedLeader/materiel-manager/manageDemandeMateriel.vue
@@ -1,17 +1,77 @@
 <template>
   <div>
-    <h1>
-        test
-    </h1>
+    <v-alert :value="alert.show" dismissible :type="alert.type">{{
+      alert.msg
+    }}</v-alert>
+    <show :demandes="demandes" @rem="rem" @setStatus="setStatus"></show>
   </div>
 </template>
 
 <script>
-export default {
+import show from "@/components/demandeMateriel/manage/show.vue";
 
-}
+export default {
+  components: {
+    show,
+  },
+  created() {
+    this.getDemandeMateriel();
+  },
+  data() {
+    return {
+      alert: {
+        show: false,
+        type: null,
+        msg: null,
+      },
+      demandes: [],
+    };
+  },
+  methods: {
+    getDemandeMateriel(alert = { show: false, type: null, msg: null }) {
+      this.$axios
+        .get("/demande-materiel")
+        .then((response) => {
+          this.demandes = response.data;
+        })
+        .catch()
+        .finally(() => {
+          this.alert = alert;
+          setTimeout(() => {
+            this.alert.show = false;
+          }, 5000);
+        });
+    },
+    rem() {},
+    setStatus(id, status) {
+      this.$axios
+        .put(`/demande-materiel-status/${id}/${status}`)
+        .then(() => {
+          if (status == 1) {
+            this.getDemandeMateriel({
+              show: true,
+              type: "success",
+              msg: "تم القبول  بنجاح",
+            });
+          }else{
+            this.getDemandeMateriel({
+              show: true,
+              type: "success",
+              msg: "تم الرفض  بنجاح",
+            });
+          }
+        })
+        .catch()
+        .finally(() => {
+          this.alert = alert;
+          setTimeout(() => {
+            this.alert.show = false;
+          }, 5000);
+        });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
