3fdba28c9e8a4191c488b1d12a6c46894d6d6ad2
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-08-05 13:28:58 +0200

    [Laravel] create model and mingration Event

---
+++ back-end/database/migrations/2022_08_05_111933_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->foreignId('troupe_id');
+            $table->foreign('troupe_id')->references('id')->on('troupes');
+            $table->integer('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
