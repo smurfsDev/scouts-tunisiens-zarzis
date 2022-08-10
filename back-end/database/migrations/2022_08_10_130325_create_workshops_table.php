fa04907836e5535369c3e54ee79b5a618b738a79
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-08-10 15:08:32 +0200

    [Laravel] create model and mogration Workshop

---
+++ back-end/database/migrations/2022_08_10_130325_create_workshops_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->datetime('heure_debut');
+            $table->datetime('heure_fin'); 
+            $table->foreignId('organisateur_id');
+            $table->foreign('organisateur_id')->references('id')->on('users');
+            $table->foreignId('event_id');
+            $table->foreign('event_id')->references('id')->on('users');
+            $table->foreignId('leader_id');
+            $table->foreign('leader_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+};
