b66a79653a50cf32e5519f504a426f6cb4d30ce3
youssef chlendi <youssef.chlendi@gmail.com>
2023-02-11 23:35:01 +0100

    download for leader

--- front-end/src/components/requestDocuments/show.vue
+++ front-end/src/components/requestDocuments/show.vue
@@ -12,14 +12,23 @@
             <v-icon v-if="item.status == 0" color="orange" v-on="on"
               >mdi-close</v-icon
             >
-            <v-icon v-else-if="item.status == 1" color="green" v-on="on"
-              >mdi-check</v-icon
+            <v-btn
+              class="mx-1"
+              v-else-if="item.status == 1"
+              @click="download(item.id)"
+              x-small
+              :fab="$vuetify.breakpoint.name != 'sm'"
+              color="success"
             >
-            <v-icon v-else color="red" v-on="on">mdi-close</v-icon>
+              <v-icon color="" v-on="on"> mdi-download </v-icon>
+            </v-btn>
+            <v-icon v-else-if="item.status == 2" color="red" v-on="on">
+              mdi-close
+            </v-icon>
           </template>
           <span v-if="item.status == 0">لم يتم الموافقة</span>
           <span v-else-if="item.status == 1">تم الموافقة</span>
-          <span v-else>تم الرفض</span>
+          <span v-else-if="item.status == 2">تم الرفض</span>
         </v-tooltip>
       </div>
     </template>
@@ -45,10 +54,43 @@
 </template>
 
 <script>
+import html2pdf from "html2pdf.js";
 import deleteDialog from "./deleteDialog.vue";
 export default {
   components: {
     deleteDialog,
+  },
+  methods: {
+    download(id) {
+      // download file usin windows
+
+      this.$axios
+        .get("/requestDocuments/" + id + "/download")
+        .then((res) => {
+          console.log(res.data);
+          html2pdf(res.data, {
+            margin: 1,
+            filename: "document.pdf",
+            image: { type: "jpeg", quality: 0.98 },
+            html2canvas: { dpi: 300, letterRendering: false, useCORS: true },
+            jsPDF: {
+              unit: "mm",
+              format: "a4",
+              orientation: "portrait",
+            },
+          });
+          this.snack = true;
+          this.snackColor = "green";
+          this.snackText = "لقد تم  التحميل";
+        })
+        .catch((error) => {
+          this.snack = true;
+          this.snackColor = "red";
+          this.snackText = error.response.data.error
+            ? error.response.data.error
+            : "لقد حصل عطب في الخادم, الرجاء المحاولة لاحقا";
+        });
+    },
   },
   props: ["request", "headers"],
 };
